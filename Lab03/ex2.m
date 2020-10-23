d1cb2258a480d4aa5a04c9513ad289b8a9ffd9d0
Gabriel de Freitas Garcia <gabrielfreitasgarcia@globo.com>
2020-10-22 23:16:07 -0300

    Separei o exercício 2 em 2

--- Lab03/ex2.m
+++ Lab03/ex2.m
@@ -1,17 +1,10 @@
-# Exercício 2
+# Exercício 2 - A
 n = 1000;
 A = rand(n,n);
-H = hilb(n);
-x = ones(n,1);
-b = A*x;
-c = H*x;
+# x = ones(n,1);
+b = A*ones(n,1);
 [La,Ua,Pa] = lu(A);
-[Lh,Uh,Ph] = lu(H);
 ya = triangInf(La,Pa,b,n);
 xa = triangSup(Ua,ya,n);
-yh = triangInf(Lh,Ph,c,n);
-xh = triangSup(Uh,yh,n);
-disp(norm(b-A*xa)/norm(xa))
+disp(norm(b-(A*xa))/norm(xa));
 disp(norm(ones(n,1)-xa)/norm(xa));
-disp(norm(c-H*xh)/norm(xh));
-disp(norm(ones(n,1)-xh)/norm(xh));
---
+++ Lab03/ex2p2.m
@@ -0,0 +1,10 @@
+#exercicio 2 - H
+n = 1000;
+H = hilb(n);
+# x = ones(n,1);
+c = H*ones(n,1);
+[Lh,Uh,Ph] = lu(H);
+yh = triangInf(Lh,Ph,c,n);
+xh = triangSup(Uh,yh,n);
+disp(norm(c-(H*xh))/norm(xh));
+disp(norm(ones(n,1)-xh)/norm(xh));
